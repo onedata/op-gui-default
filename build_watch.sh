f119c12073da388840463f1ca6fa2c8ed6957e35
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-04-25 18:29:06 +0200

    VFS-1825 improve some comments

--- build_watch.sh
+++ build_watch.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+#####################################################################
+# @author Lukasz Opiola
+# @copyright (C): 2016 ACK CYFRONET AGH
+# This software is released under the MIT license
+# cited in 'LICENSE.txt'.
+#####################################################################
+# usage:
+# ./build_watch.sh
+#
+# This script starts an ember build process that watches for changes in
+# src dir and automatically rebuilds GUI release.
+#####################################################################
+
 SRC_DIR=src
 REL_DIR=rel
 

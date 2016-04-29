c51c67b2c2b488878dcc0434baf0e28c12ad4cc4
Jakub Liput <jakub.liput@gmail.com>
2016-04-29 15:21:21 +0200

    VFS-1939: Removing dev code

--- src/app/models/file.js
+++ src/app/models/file.js
@@ -22,8 +22,7 @@ export default DS.Model.extend({
     Specifies is this object a regular file ("file") or directory ("dir")
     To check if it is a dir please use "isDir" property.
   */
-  // type: DS.attr('string'),
-  type: 'broken',
+  type: DS.attr('string'),
   parent: DS.belongsTo('file', {inverse: 'children', async: true}),
   children: DS.hasMany('file', {inverse: 'parent', async: true}),
 

d665b6344cd22c2215c06175024b908596c0b1b1
Jakub Liput <jakub.liput@gmail.com>
2016-06-27 14:01:15 +0200

    Squashed 'gui/' changes from d8c8833..1bc5be4
    1bc5be4 VFS-2148 Updated bind-floater
    
    git-subtree-dir: gui
    git-subtree-split: 1bc5be4cef9726d52022cc6af52449ce1ab2feb2

--- common_assets/js/utils/bind-floater.js
+++ common_assets/js/utils/bind-floater.js
@@ -1,19 +1,24 @@
-// jshint esnext: true
-
 /**
   Makes an element fixed positioned, with coordinates (left, top) of its parent.
-  Please use returned function to recompute fixed position on your events (eg. scroll).
 
-  @param element A jQuery element which will float (only single!)
-  @param parent (optional) A jQuery element which will act as element position parent.
-          If not provided - will use a element.parent().
+  @param element A jQuery element which position will be computed in relation to "parent"
+  @param {object} [parent=element.parent()] A jQuery element which will act as element position parent.
+          If null or undefined - will use a element.parent().
   @param {object} options Additional options to manipulate floater behaviour, properties:
-            - posX {string} right/left - the floater will position on right/left of parent
-                (default: right)
-            - posX {string} top/middle - the floater (0,0) will position on top/middle of parent
+  @param {string} [options.posX=right] horizontal position of element relative to the parent, possible:
+            - ``left`` - the element will be placed completely on left of the parent
+            - ``right`` - the element will be placed completely on right of the parent
+  @param {string} [options.posY=top] vertical position of element relative to the parent, possible:
+            - ``top`` - the element will be placed completely on top of the parent
+            - ``top-above`` - same as ``top``
+            - ``top-middle`` - the vertical middle of element will be placed on top of parent
+            - ``middle`` - the vertical middle of element will be placed in the vertical middle of parent
+            - ``middle-middle`` - same as ``middle``
+            /middle - the floater (0,0) will position on top/middle of parent
                 (default: top)
-            - offsetX {integer} - X offset of (0,0) position in px
-            - offsetY {integer} - X offset of (0,0) position in px
+  @param {integer} [offsetX=0] X offset of computed position in px
+  @param {integer} [offsetY=0] Y offset of computed position in px
+
   @returns {function} Function which re-computes new fixed position of an element.
               It should be bind to eg. parent mouseover.
  */
@@ -34,12 +39,16 @@ export default function bindFloater(element, parent, options) {
       left = `${parseInt(offset.left) + parent.width()}px`;
     } else if (options.posX === 'left') {
       left = `${parseInt(offset.left) - element.width()}px`;
+    } else if (options.posX === 'center') {
+      left = `${parseInt(offset.left) + parent.width()/2 - element.width()/2}px`;
     }
     let top;
-    if (options.posY === 'top') {
+    if (options.posY === 'top' || options.posY === 'top-above') {
       top = offset.top;
-    } else if (options.posY === 'middle') {
+    } else if (options.posY === 'top-middle') {
       top = `${parseInt(offset.top) - element.height()/2}px`;
+    } else if (options.posY === 'middle' || options.posY === 'middle-middle') {
+      top = `${parseInt(offset.top) + parent.height()/2 - element.height()/2}px`;
     }
 
     element.css({

a941269b9a5507a377f7e0f714d64403cea71d78
Jakub Liput <jakub.liput@gmail.com>
2016-10-07 12:27:32 +0200

    VFS-2660 Fixes in auto-positioning resize handler

--- src/app/components/data-files-tree.js
+++ src/app/components/data-files-tree.js
@@ -23,17 +23,41 @@ export default Ember.Component.extend({
   rootDir: null,
 
   didInsertElement() {
+    this.bindResizeHandler();
+    $(window).on('resize', this.get('updateResizeHandlerPositionFun'));
+  },
+
+  willDestroyElement() {
+    $(window).off('resize', this.get('updateResizeHandlerPositionFun'));
+  },
+
+  updateResizeHandlerPositionFun: Ember.computed(function() {
+    const $resizeHandler = $('#data-sidebar-resize-handler');
+    const $secondarySidebar = $('.secondary-sidebar');
+    return function() {
+      $resizeHandler.css('left', $secondarySidebar.width() - $resizeHandler.width()/2);
+    };
+  }),
+
+  /**
+   * Enables "resize handler" - a invisible separator, that allows to change
+   * width of secondary-sidebar.
+   */
+  bindResizeHandler() {
     const handleSelector = '#data-sidebar-resize-handler';
     const $resizeHandler = $(handleSelector);
-    const $secondarySidebar = $('.secondary-sidebar');
     // resize handler is positioned absolutely to main-content
-    $resizeHandler.css('left', $secondarySidebar.width() - $resizeHandler.width()/2);
+    this.get('updateResizeHandlerPositionFun')();
+    const self = this;
     $('.secondary-sidebar').resizable({
       handleSelector: handleSelector,
       resizeHeight: false,
       onDrag(e, $el, newWidth/*, newHeight, opt*/) {
         $resizeHandler.css('left', newWidth - $resizeHandler.width()/2);
-      }
+      },
+      onDragEnd(/*e, $el, opt*/) {
+        self.get('updateResizeHandlerPositionFun')();
+      },
     });
   },
 

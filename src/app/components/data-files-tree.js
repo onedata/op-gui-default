e846eefa46dc26606ab3be693e467ea61e02eda6
Jakub Liput <jakub.liput@gmail.com>
2016-10-07 11:38:32 +0200

    VFS-2660 Cleaning up obsolete "data-files-tree service API"

--- src/app/components/data-files-tree.js
+++ src/app/components/data-files-tree.js
@@ -37,23 +37,6 @@ export default Ember.Component.extend({
     });
   },
 
-  /*** Bind with main-menu service, TODO: use mixin or something? ***/
-  SERVICE_API: ['setRootDir'],
-
-  /** Listen on mainMenuService's events */
-  listen: function() {
-    let fileSystemTreeService = this.get('fileSystemTreeService');
-    this.SERVICE_API.forEach(name => fileSystemTreeService.on(name, this, name));
-  }.on('init'),
-
-  /** Deregister event listener from main-menu service */
-  cleanup: function() {
-    let fileSystemTreeService = this.get('fileSystemTreeService');
-    this.SERVICE_API.forEach(name => fileSystemTreeService.off(name, this, name));
-  }.on('willDestroyElement'),
-
-  /*** Service API ***/
-
   actions: {
     /** Typically invoked by actions passed up from tree nodes */
     openDirInBrowser(file) {

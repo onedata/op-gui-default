9a453c17a6a5d0a560c73fdfc413685d5c6f2f0c
Jakub Liput <jakub.liput@gmail.com>
2016-12-13 11:52:38 +0100

    VFS-2417 Removing bootstrap-sass from bower It is unnecessary because ember bootstrap packages are used with npm

--- src/app/app.js
+++ src/app/app.js
@@ -7,8 +7,8 @@
 */
 
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from 'ember-resolver';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 let App;

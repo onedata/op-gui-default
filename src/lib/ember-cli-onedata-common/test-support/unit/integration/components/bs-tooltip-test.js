471e5e382b1b6e83da7d9318c805c60c22a985b3
Jakub Liput <jakub.liput@gmail.com>
2017-01-04 11:14:46 +0100

    VFS-2921 Fixing deprecation in onedata-common tests

--- src/lib/ember-cli-onedata-common/test-support/unit/integration/components/bs-tooltip-test.js
+++ src/lib/ember-cli-onedata-common/test-support/unit/integration/components/bs-tooltip-test.js
@@ -1,37 +1,32 @@
 /* jshint expr:true */
 import { expect } from 'chai';
-import {
-  describeComponent,
-  it
-} from 'ember-mocha';
+import { it, describe } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describeComponent(
-  'bs-tooltip',
-  'Integration: BsTooltipComponent',
-  {
+describe('Integration: BsTooltipComponent', function() {
+  setupComponentTest('bs-tooltip', {
     integration: true
-  },
-  function() {
-    it('generates element with data-toggle and data-original-title', function() {
-      this.render(hbs`
-        {{#bs-tooltip class="test-bs-tooltip" title="Hello world"}}
-          Something different
-        {{/bs-tooltip}}
-      `);
-      let $tooltip = this.$().find('.test-bs-tooltip');
-      expect($tooltip).to.have.attr('data-toggle', 'tooltip');
-      expect($tooltip).to.have.attr('data-original-title', 'Hello world');
-    });
+  });
 
-    it('yields inner content without modifications', function() {
-      this.render(hbs`
-        {{#bs-tooltip class="test-bs-tooltip" title="Hello world"}}
-          <div>inner content</div>
-        {{/bs-tooltip}}
-      `);
-      let $tooltip = this.$().find('.test-bs-tooltip');
-      expect($tooltip.html()).to.match(/\s*<div>inner content<\/div>\s*/);
-    });
-  }
-);
+  it('generates element with data-toggle and data-original-title', function() {
+    this.render(hbs`
+      {{#bs-tooltip class="test-bs-tooltip" title="Hello world"}}
+        Something different
+      {{/bs-tooltip}}
+    `);
+    let $tooltip = this.$().find('.test-bs-tooltip');
+    expect($tooltip).to.have.attr('data-toggle', 'tooltip');
+    expect($tooltip).to.have.attr('data-original-title', 'Hello world');
+  });
+
+  it('yields inner content without modifications', function() {
+    this.render(hbs`
+      {{#bs-tooltip class="test-bs-tooltip" title="Hello world"}}
+        <div>inner content</div>
+      {{/bs-tooltip}}
+    `);
+    let $tooltip = this.$().find('.test-bs-tooltip');
+    expect($tooltip.html()).to.match(/\s*<div>inner content<\/div>\s*/);
+  });
+});
--- src/lib/ember-cli-onedata-common/test-support/unit/integration/components/one-icon-test.js
+++ src/lib/ember-cli-onedata-common/test-support/unit/integration/components/one-icon-test.js
@@ -1,29 +1,24 @@
 /* jshint expr:true */
 import { expect } from 'chai';
-import {
-  describeComponent,
-  it
-} from 'ember-mocha';
+import { it, describe } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describeComponent(
-  'one-icon',
-  'Integration: OneIconComponent',
-  {
+describe('Integration: OneIconComponent', function() {
+  setupComponentTest('one-icon', {
     integration: true
-  },
-  function() {
-    it('renders element with oneicon-<name> provided with icon property', function() {
-      this.render(hbs`{{one-icon icon="space"}}`);
-      let $oneicon = this.$().find('.one-icon');
-      expect($oneicon).to.have.class('oneicon-space');
-    });
-    it('propagates passed classes and preserves icon class', function() {
-      this.render(hbs`{{one-icon icon="space" class="some-class other-class"}}`);
-      let $oneicon = this.$().find('.one-icon');
-      expect($oneicon).to.have.class('oneicon-space');
-      expect($oneicon).to.have.class('some-class');
-      expect($oneicon).to.have.class('other-class');
-    });
-  }
-);
+  });
+
+  it('renders element with oneicon-<name> provided with icon property', function() {
+    this.render(hbs`{{one-icon icon="space"}}`);
+    let $oneicon = this.$().find('.one-icon');
+    expect($oneicon).to.have.class('oneicon-space');
+  });
+  it('propagates passed classes and preserves icon class', function() {
+    this.render(hbs`{{one-icon icon="space" class="some-class other-class"}}`);
+    let $oneicon = this.$().find('.one-icon');
+    expect($oneicon).to.have.class('oneicon-space');
+    expect($oneicon).to.have.class('some-class');
+    expect($oneicon).to.have.class('other-class');
+  });
+});

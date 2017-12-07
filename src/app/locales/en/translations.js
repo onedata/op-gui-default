9aa2bd1bada0c91b4059c0841562e9adadd832b3
Jakub Liput <jakub.liput@gmail.com>
2017-12-07 15:52:42 +0100

    VFS-3880 Showing error message in throughput chart if current stats cannot be loaded

--- src/app/locales/en/translations.js
+++ src/app/locales/en/translations.js
@@ -498,6 +498,7 @@ export default {
       completedTransfers: 'History of transfers',
       noActiveTransfers: 'There are no active transfers',
       noCompletedTransfers: 'There are no past transfers',
+      throughputChartError: 'Failed to load all the data for the chart',
       initializingTransfers: 'Initializing transfers...',
       notSupported: 'Cannot list transfers of selected space because it is not ' +
         'supported by current provider',

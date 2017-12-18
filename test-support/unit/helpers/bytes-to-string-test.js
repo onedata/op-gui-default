958d6a1035039e92ff0d46e025c5e3a338e487f6
Jakub Liput <jakub.liput@gmail.com>
2017-12-18 09:46:59 +0100

    Squashed 'src/lib/ember-cli-onedata-common/' changes from 612a451..da57cfc
    da57cfc Merge pull request #13 in VFS/ember-cli-onedata-common from feature/VFS-3710-unify-bytes-units to develop
    c2d93c6 Merge commit '8b8d5ced6e89fc3ffe7cd08deecb72d0fbcbbee2' into feature/VFS-3710-unify-bytes-units
    d732ba6 VFS-3710 Porting extensions for bytes-to-string; making IEC default
    
    git-subtree-dir: src/lib/ember-cli-onedata-common
    git-subtree-split: da57cfc5472abc665a0230b978a177eb230ce27e

--- test-support/unit/helpers/bytes-to-string-test.js
+++ test-support/unit/helpers/bytes-to-string-test.js
@@ -2,15 +2,21 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { bytesToString } from 'ember-cli-onedata-common/helpers/bytes-to-string';
 
-describe('Unit | Helper | bytes to string', function() {
-  it('generates valid KB string as in bytes-to-string util', function() {
-    let result = bytesToString([1500]);
-    expect(result).to.be.equal('1.5 KB');
-  });
-
-  it('supports iecFormat option of bytes-to-string util', function() {
-    let result = bytesToString([1024], {iecFormat: true});
+describe('Unit | Helper | bytes to string', function () {
+  it('supports iecFormat option of bytes-to-string util', function () {
+    let result = bytesToString([1024], { iecFormat: true });
     expect(result).to.be.equal('1 KiB');
   });
-});
 
+  it('uses iecFormat=true option of bytes-to-string util by default', function () {
+    let result = bytesToString([1024]);
+    expect(result).to.be.equal('1 KiB');
+  });
+
+  it('generates valid KB string with iecFormat=false as in bytes-to-string util',
+    function () {
+      let result = bytesToString([1500], { iecFormat: false });
+      expect(result).to.be.equal('1.5 KB');
+    }
+  );
+});
--- test-support/unit/utils/bytes-to-string-test.js
+++ test-support/unit/utils/bytes-to-string-test.js
@@ -1,31 +1,30 @@
-/* jshint expr:true */
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import bytesToString from 'ember-cli-onedata-common/utils/bytes-to-string';
 
-describe('Unit | Utility | bytes to string', function() {
-  it('can convert byte size to GiB string', function() {
-    let result = bytesToString(1073741824, {iecFormat: true});
+describe('Unit | Utility | bytes to string', function () {
+  it('can convert byte size to GiB string', function () {
+    let result = bytesToString(1073741824, { iecFormat: true });
     expect(result).to.be.equal('1 GiB');
   });
 
-  it('can convert byte size to MiB string', function() {
-    let result = bytesToString(1048576, {iecFormat: true});
+  it('can convert byte size to MiB string', function () {
+    let result = bytesToString(1048576, { iecFormat: true });
     expect(result).to.be.equal('1 MiB');
   });
 
-  it('can convert byte size to GB string with default options', function() {
-    let result = bytesToString(1000000000);
+  it('can convert byte size to GB string', function () {
+    let result = bytesToString(1000000000, { iecFormat: false });
     expect(result).to.be.equal('1 GB');
   });
 
-  it('can convert byte size to MB string with default options', function() {
-    let result = bytesToString(1000000);
+  it('can convert byte size to MB string', function () {
+    let result = bytesToString(1000000, { iecFormat: false });
     expect(result).to.be.equal('1 MB');
   });
 
-  it('rounds size in string to 1 digit after comma by default', function() {
-    let result = bytesToString(1251111);
+  it('rounds size in string to 1 digit after comma by default (w/o IEC)', function () {
+    let result = bytesToString(1251111, { iecFormat: false });
     expect(result).to.be.equal('1.3 MB');
   });
 });

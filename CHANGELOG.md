a84b088af1be618c7959b8f756807104ee95b68c
Michal Borzecki <michal.borzecki@hotmail.com>
2018-02-09 09:34:23 +0100

    VFS-4068 Fixed incorrect icons positioning in transfers table

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ CHANGELOG
 
 ### Latest changes
 
+* VFS-4068 Fixed incorrect icons positioning in transfers table
 * VFS-4062 Remember opened space when switching between data-spaces-transfers views; fixes in data-space sidebar
 * VFS-4059 Fixed provider icon scaling in transfers view
 * VFS-4002 Showing transfer type (replication/migration/invalidation) in transfers table
--- src/app/components/transfers/live-stats-table/cell-errorable.js
+++ src/app/components/transfers/live-stats-table/cell-errorable.js
@@ -3,13 +3,14 @@
  * Cell component for `models-table` in `live-stats-table`.
  *
  * @module components/live-stats-table/cell-errorable
- * @author Jakub Liput
- * @copyright (C) 2017 ACK CYFRONET AGH
+ * @author Jakub Liput, Michal Borzecki
+ * @copyright (C) 2017-2018 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'span',
   classNames: ['cell-errorable'],
 });

5f554140d249e477f934cd5e843e7db9294b25f7
Michal Borzecki <michal.borzecki@hotmail.com>
2018-04-17 13:14:48 +0200

    VFS-4355 customization of transfer throughput chart labels

--- src/app/locales/en/translations.js
+++ src/app/locales/en/translations.js
@@ -561,6 +561,8 @@ export default {
         title: 'Providers throughput',
         input: 'Input',
         output: 'Output',
+        receivedFrom: 'Received from',
+        sentTo: 'Sent to',
         timeLastUpdate: 'Time (last update: {{lastUpdate}})',
         throughput: 'Throughput',
         onTheFly: 'On-the-fly',
